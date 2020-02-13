bd7c2bd87ad4226db01b5e3f20ea1950d8bceb94
hritikkothari <kotharihrithik1@gmail.com>
2020-02-13 16:40:40 +0530

    Exp4 updated

--- Python/exp4/dict.py
+++ Python/exp4/dict.py
@@ -1,55 +1,29 @@
 
 def generate_dict(n):
-    """
-    Generate dict n:n*2
-    """
+    d1 = dict()
     for i in range(1,n+1):
-        user_dict[i]=i*i
+        d1[i]=i*i
 
-    print(user_dict)
+    return d1
 
-def merge_dict():
-    """
-    Merging two dicts
-    """
-    user_dict2 = dict()
-    nt = int(input("Enter value of n for second dict"))
+def merge_dict(d1,d2):
+    d1.update(d2)
+    print("After merging : \n",d1)
 
-    for i in range(n,nt+1):
-        user_dict2[i]=i*i
-        
-    user_dict.update(user_dict2)
-    print("After merging : \n",user_dict)
+def sort_dict(d1):
+    print("Sorted Dict \n",dict(sorted(d1.items())))
 
+def remove_key(d1,k):
+    d1.pop(k)
+    print("After removing: \n", d1)
 
-def sort_dict():
-    """
-    Sorting a dict
-    """
-    random_dict={1:1, 9:3, 16:4, 20:2, 55:5, 0:0}
-    print("Sorted Dict \n",dict(sorted(random_dict.items())))
-
-def remove_key(k):
-    """
-    Remove a key from the dict
-    """
-    user_dict.pop(k)
-    print("After removing: \n", user_dict)
-
-def unique_val():
-    """
-    Convert dict values to a set to get unique values
-    """
-    temp_set = set(user_dict.values())
-    print("Unique values \n", temp_set)
+def unique_val(d1):
+     print(set(d1.values()))
     
-def highest_three():
-    """
-    Get highest 3 vals in the dict
-    """
-    print("Highest 3 vals are \n",sorted(user_dict.items(), reverse=True)[:3])
+def highest_three(d1):
+    print(sorted(d1.items(), reverse=True)[:3])
 
-def count_freq(user_str):
+def count_freq(d1,user_str):
     word_list = list(user_str.split())
     freq_list = []
     for i in word_list:
@@ -57,15 +31,15 @@ def count_freq(user_str):
     count_dict = dict(zip(word_list, freq_list))
     print(count_dict)
         
-user_dict = dict()
 
 n = int(input("Enter value of n"))
-generate_dict(n)
+d1=generate_dict(n)
+print(d1)
 
-merge_dict()
-sort_dict()
-remove_key(5)
-highest_three()
+merge_dict(d1,{6:36,7:49,8:64})
+sort_dict(d1)
+remove_key(d1,5)
+highest_three(d1)
 
 user_str = input("Enter string to count freq of words")
-count_freq(user_str)
+count_freq(d1, user_str)
