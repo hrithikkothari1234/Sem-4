6e24a1b0bf887eeadc5576fc286a3f54cb490256
hritikkothari <kotharihrithik1@gmail.com>
2020-03-20 15:59:27 +0530

    updated

--- Python/Ques Bank/all.py
+++ Python/Ques Bank/all.py
@@ -80,26 +80,24 @@ print(sublist([3],[2,2,3,4,5]))
 # Q5
 
 user_str = input("Enter 2*n lines")
-l1=[]
-l2=[]
+l=[]
 
 while user_str!='':
-    l1.append(user_str)
+    l.append(user_str)
     user_str=input()
 
-if len(l1)%2!=0:
+if len(l)%2!=0:
     print("Enter 2*n lines plis?")
 else:
-    for i in range(0, len(l1), 2):
-        l2.append(l1[i])
-        l1.remove(l1[i])
-
-    l1.reverse()
-    l2.reverse()
-
-    for x,y in zip(l1, l2):
-        print(x)
-        print(y)
+    n=len(l)  
+    a=int(n/2)
+    l1=l[a:]
+    l2=l[:a]
+    
+    for i in l1:
+        print(i)
+    for i in l2:
+        print(i)
 
 # Q6
 
