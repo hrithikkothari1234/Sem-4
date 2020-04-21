2505e5cb55e946f30a9a84526d40a94b705bd95b
hritikkothari <kotharihrithik1@gmail.com>
2020-04-21 14:14:49 +0530

    exp14

---
+++ Python/exp14/client.py
@@ -0,0 +1,17 @@
+import socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print("__CLIENT__ side...")
+
+while True:
+    msg = input('CLIENT : ')
+    client_socket.sendto(msg.encode('utf-8'), (socket.gethostname(), 7765))
+    print('sent')
+
+    data, address = client_socket.recvfrom(1024)
+    print('--> server says :')
+
+    print(str(data))
+    client_socket.sendto(bytes(msg, 'utf-8'), address)
+    print('sent')
+    
